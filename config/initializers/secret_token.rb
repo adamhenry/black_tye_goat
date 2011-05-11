5b587f6dfca12c1ed1044e4a9945b4ed235e151f
Adam Henry <a.david.henry@gmail.com>
2011-05-10 18:34:56 -0700

    scaffolded customer

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BlackTyeGoat::Application.config.secret_token = 'c85c745bcfdad9955665927b94d3dfa9deef4870dd8e5c54a2838d5bcc74199100f842aaf2fefac73ac70dd26d3fb196a8dd150ffe6c55d23d53247eae2de6f6'
